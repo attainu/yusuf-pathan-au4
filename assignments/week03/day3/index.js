1557e78642eba41b4f7978b0354e344202ed2884
Yusuf Pathan <yusufp04@gmail.com>
2019-09-25 15:13:28 +0530

    created a js file
    this file prints the table of five using a for loop inside a function

---
+++ assignments/week03/day3/index.js
@@ -0,0 +1,7 @@
+// using a for loop inside a function to print table of five
+function tables() {
+    for (var i = 1; i <= 10; i++){
+        console.log(i+" "+"*"+" "+"5"+" "+"="+" "+(i*5));
+    }
+}
+tables();
