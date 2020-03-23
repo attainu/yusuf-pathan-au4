bc54c6e75cb53e0668210514d79de7ae52044e68
Yusuf Pathan <yusufp04@gmail.com>
2020-03-23 10:50:15 +0530

    assignment

---
+++ assignments/week27/day3/script.js
@@ -0,0 +1,23 @@
+// // Ways of Rendering hello world using react
+// let rootElement = document.getElementById("root");
+// let hello = React.createElement("span", { className:"myclass" }, "Hello world");    
+// console.log(hello)
+// // rootElement.appendChild(hello);  // Method 1
+// ReactDOM.render(hello, rootElement)
+
+const rootElement = document.getElementById("root");
+const hello = <span> Hello World</span>
+const button = <button>Add</button>
+ReactDOM.render(
+    <div> 
+        {hello}
+        {button}
+    </div>,
+    rootElement);
+
+ var aa = 10; 
+ sayhello (name) {
+     
+ }
+
+// To render variable in jsx syntax we have to use {}
