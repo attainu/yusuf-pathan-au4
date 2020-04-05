5253556b6f8c8b9bb147b7a63577e1efbb361e44
Yusuf Pathan <yusufp04@gmail.com>
2020-04-05 21:42:00 +0530

    assignments and coding challenges

---
+++ assignments/week27/day5/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app.js'
+import "./header.css";
+import "./footer.css";
+
+ReactDOM.render(<App />, document.getElementById('app'));
